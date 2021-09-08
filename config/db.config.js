2751d8d3a2821ab4c828534c87c7e898368535e8
Nisar Shaikh <>
2021-09-08 08:30:03 +0530

    added PORT for db from process.env

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,16 @@
 const { createPool } = require("mysql");
 /** Connection pool creation - START */
+const host = process.env.DB_HOST || "localhost";
+const user = process.env.DB_USER || "root";
+const password = process.env.DB_PASSWORD || "password";
+const name = process.env.DB_NAME || "node-post-api";
+
 const db = createPool({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "node-post-api",
+  host: host,
+  user: user,
+  password: password,
+  database: name,
   connectionLimit: 10,
 });
 /** Connection pool creation - END */

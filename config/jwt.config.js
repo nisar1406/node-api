b94f7d73110b88ce22da176ea34f71fd8b69cde9
Nisar Shaikh <>
2021-09-08 15:11:54 +0530

    added jwt configurations, modifies all user realted files

---
+++ config/jwt.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: 'This is a secret key, please do not share!',
+  expireTime: 86400,
+};
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const swaggerJsdoc = require("swagger-jsdoc");
 const userRoutes = require('./routes/user.route');
 const postRoutes = require('./routes/post.route');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(bodyParser.json());
